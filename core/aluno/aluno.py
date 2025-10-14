5ba2ad8a98647a336f3d78e38cd0b77e6f2a9674
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-14 20:17:35 -0300

    nova auteração

--- core/aluno/aluno.py
+++ core/aluno/aluno.py
@@ -3,3 +3,26 @@ class Aluno:
         self.nome = nome
         self.id = id
         self.rm = rm
+
+@property
+def nome(self):
+    return self.__nome
+@nome.setter
+def nome(self, nome):
+    self.__nome = nome
+
+
+@property
+def id(self):
+    return self.__id  
+@id.setter
+def set_id(self, id):
+    self.__id = id
+
+
+@property
+def rm(self):
+    return self.__rm 
+@rm.setter
+def set_rm(self, rm):
+    self.__rm = rm 

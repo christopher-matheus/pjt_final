a4821d9839b660a8dee41972653315ac27ca326b
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-14 20:03:27 -0300

    nova auteração

---
+++ core/aluno/aluno.py
@@ -0,0 +1,5 @@
+class Aluno:
+    def __init__(self, nome, id, rm):
+        self.nome = nome
+        self.id = id
+        self.rm = rm
---
+++ core/professor/professor.py
@@ -0,0 +1,5 @@
+class Professor:
+    def __init__(self, nome, id, disciplina):
+        self.nome = nome
+        self.id = id
+        self.disciplina = disciplina

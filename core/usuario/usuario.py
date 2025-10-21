0fbab496ac13a83e777e551c0b44075df0968c55
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-21 19:31:33 -0300

    nova auteração

--- core/usuario/usuario.py
+++ core/usuario/usuario.py
@@ -1,28 +1,36 @@
 class Usuario:
-    def __init__(self, nome, id, email):
+    def __init__(self, nome, id, ativo, senha):
         self.nome = nome
         self.id = id
-        self.email = email
+        self.senha = senha
+        self.ativo = ativo
 
     @property
     def nome(self):
         return self._nome
     @nome.setter
-    def nome(self, valor):
-        self._nome = valor
+    def nome(self, n_nome):
+        self._nome = n_nome
 
 
     @property
     def id(self):
         return self._id
     @id.setter
-    def id(self, valor):
-        self._id = valor
+    def id(self, n_id):
+        self._id = n_id
 
-        
+
     @property
-    def email(self):
-        return self._email 
-    @email.setter
-    def email(self, valor):
-        self._email = valor
+    def ativo(self):
+        return self._ativo 
+    @ativo.setter
+    def ativo(self, n_ativo):
+        self._ativo = n_ativo
+
+    @property
+    def senha(self):
+        return self._senha
+    @senha.setter
+    def senha(self, n_senha):
+        self._senha = n_senha

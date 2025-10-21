6a750180ba52c481067978c43cb06d6553b204a6
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-21 19:29:02 -0300

    nova auteração

--- core/usuario/usuario.py
+++ core/usuario/usuario.py
@@ -3,3 +3,26 @@ class Usuario:
         self.nome = nome
         self.id = id
         self.email = email
+
+    @property
+    def nome(self):
+        return self._nome
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
+
+
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, valor):
+        self._id = valor
+
+        
+    @property
+    def email(self):
+        return self._email 
+    @email.setter
+    def email(self, valor):
+        self._email = valor

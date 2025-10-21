a4f240547b9123563353fa3fff05e78e5d020572
christopher-matheus <christophermsmoreira@gmail.com>
2025-10-21 20:24:30 -0300

    nova auteração

--- core/professor/professor.py
+++ core/professor/professor.py
@@ -3,3 +3,27 @@ class Professor:
         self.nome = nome
         self.id = id
         self.disciplina = disciplina
+
+
+    @property
+    def usuario(self):
+        return self._usuario
+    @usuario.setter
+    def usuario(self, n_usuario):
+        self._usuario = n_usuario
+
+
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, n_id):
+        self._id = n_id
+
+
+    @property
+    def disciplina(self):
+        return self._disciplina 
+    @disciplina.setter
+    def disciplina(self, n_disciplina):
+        self._disciplina = n_disciplina
